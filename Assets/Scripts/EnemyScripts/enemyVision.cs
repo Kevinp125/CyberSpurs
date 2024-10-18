90855a3cc7104be61beab03acedbdcdbf14bb089
Daniel <danielperera0111@gmail.com>
2024-10-18 15:13:54 -0400

    In the process of adding enemy movement

--- Assets/Scripts/EnemyScripts/enemyVision.cs
+++ Assets/Scripts/EnemyScripts/enemyVision.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class enemyVision : MonoBehaviour
 {
     static public bool found = false;
-
+    public bool found2 = false;
     public float lookRadius;
 
     void Update()
@@ -17,11 +17,13 @@ public class enemyVision : MonoBehaviour
             if (nearbyObject.tag == "Player")
             {
                 found = true;
+                found2 = true;
             }
 
             else
             {
                 found = false;
+                found2 = false;
             }
         }
 
