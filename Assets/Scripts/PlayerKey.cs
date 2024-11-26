5e68abf68f82513f124c05368ec02ec7cbbab28f
Daniel <danielperera0111@gmail.com>
2024-11-25 19:24:45 -0500

    Reorganized all the newly added enemies and decorations and made some enemies only be activated once the key is grabbed

--- Assets/Scripts/PlayerKey.cs
+++ Assets/Scripts/PlayerKey.cs
@@ -8,6 +8,7 @@ public class PlayerKey : MonoBehaviour
     public GameObject door;
     public LayerMask layerIgnore;
     public bool haveKey;
+    public GameObject[] enemies;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +23,11 @@ public class PlayerKey : MonoBehaviour
             Destroy(key);
             Destroy(door);
             haveKey = true;
+        }
+
+        foreach (GameObject enemy in enemies)
+        {
+            enemy.SetActive(true); // Activate each enemy
         }
     }
 }
